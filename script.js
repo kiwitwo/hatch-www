cedab8661dbadf969bd857d54045c1dc9445bea3
han <achhmd25@gmail.com>
2024-12-08 15:26:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function dropToggle() {
+  var x = document.getElementById("userdrop");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
