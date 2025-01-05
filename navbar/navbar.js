0a55392b15fe6452a697ac8c3c5f02513a3565be
jabinstech <jabinstech.net@gmail.com>
2025-01-05 17:17:08 -0500

    REMOVE HATCH-WWW :yippee:

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,8 +1,8 @@
 document.getElementById("navarea").innerHTML = `<div class="navbg"></div>
         <div class="nav">
-          <div id="logo"><img class="logo" src="https://hatchdotlol.github.io/hatch-www/navbar/img/logo.png" height:30px onclick="location.href='index.html'" /></div>
+          <div id="logo"><img class="logo" src="https://hatchdotlol.github.io/navbar/img/logo.png" height:30px onclick="location.href='index.html'" /></div>
           <div class="searchimgnav" onclick="searchMade()">
-            <img src="https://hatchdotlol.github.io/hatch-www/navbar/img/search.png" id="searchimghard"/>
+            <img src="https://hatchdotlol.github.io/navbar/img/search.png" id="searchimghard"/>
           </div>
           <div class="searchnavitem" id="searchdiv">
             <input type="text" id="searchinp" name="search" placeholder=" Search...">
@@ -20,11 +20,11 @@ document.getElementById("navarea").innerHTML = `<div class="navbg"></div>
             Explore
           </a>
           <a class="navitem" id="mail" href="/messages.html">
-            <img src="https://hatchdotlol.github.io/hatch-www/navbar/img/messages.svg" id="msgsym" /><div id="messagect"><b>1</b></div>
+            <img src="https://hatchdotlol.github.io/navbar/img/messages.svg" id="msgsym" /><div id="messagect"><b>1</b></div>
         
           </a>
           <div id="userbox">
-          <div id="userinfo"><img id="pfpnav" src="https://uploads.scratch.mit.edu/get_image/user/41022482_96x96.png"><div id="usernamenav">han614698</div><div id="dropdownimgcover"><img id="dropdownarrow" src="https://hatchdotlol.github.io/hatch-www/navbar/img/downarrow.png" /></div></div>
+          <div id="userinfo"><img id="pfpnav" src="https://uploads.scratch.mit.edu/get_image/user/41022482_96x96.png"><div id="usernamenav">han614698</div><div id="dropdownimgcover"><img id="dropdownarrow" src="https://hatchdotlol.github.io/navbar/img/downarrow.png" /></div></div>
             <div id="userdrop" style="display: none" tabindex="0">
               <a class="dropdown-option" id="highest" href="/profile?id=yourUsername">
                 Your Profile
