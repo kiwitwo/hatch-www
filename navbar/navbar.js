e7da85d58173112691b27617b0cd33f4bfbf5b68
han <achhmd25@gmail.com>
2025-01-05 15:18:12 -0600

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -29,3 +29,45 @@ searchInp.onkeydown = (e) => {
         searchMade()
     }
 }
+
+
+ document.getElementById("footer").innerHTML = `<div class="navbg"></div>
+        <div class="nav">
+          <div id="logo"><img class="logo" src="https://hatchdotlol.github.io/hatch-www/navbar/img/logo.png" height:30px onclick="location.href='index.html'" /></div>
+          <div class="searchimgnav" onclick="searchMade()">
+            <img src="https://hatchdotlol.github.io/hatch-www/navbar/img/search.png" id="searchimghard"/>
+          </div>
+          <div class="searchnavitem" id="searchdiv">
+            <input type="text" id="searchinp" name="search" placeholder=" Search...">
+          </div>
+          <a class="navitem" href="/wiki.html">
+            Wiki
+          </a>
+          <a class="navitem" href="/discuss.html">
+            Discuss
+          </a>
+          <a class="navitem" href="/about.html">
+            About
+          </a>
+          <a class="navitem" href="/explore.html">
+            Explore
+          </a>
+          <a class="navitem" id="mail" href="/messages.html">
+            <img src="https://hatchdotlol.github.io/hatch-www/navbar/img/messages.svg" id="msgsym" /><div id="messagect"><b>1</b></div>
+        
+          </a>
+          <div id="userbox">
+          <div id="userinfo"><img id="pfpnav" src="https://uploads.scratch.mit.edu/get_image/user/41022482_96x96.png"><div id="usernamenav">han614698</div><div id="dropdownimgcover"><img id="dropdownarrow" src="https://hatchdotlol.github.io/hatch-www/navbar/img/downarrow.png" /></div></div>
+            <div id="userdrop" style="display: none" tabindex="0">
+              <a class="dropdown-option" id="highest" href="/profile?id=yourUsername">
+                Your Profile
+              </a>
+              <a class="dropdown-option" href="/settings">
+                Account Settings
+              </a>
+              <div onclick="alert('this will probably do something eventually')" class="dropdown-option" id="lowest">
+                Sign Out
+              </div>
+            </div>
+          </div>
+        </div>`;
