36db551ef46d2642911c2f42aade75e2cdd601e4
jabinstech <jabinstech.net@gmail.com>
2025-01-05 17:17:08 -0500

    REMOVE HATCH-WWW :yippee:

--- footer/footer.js
+++ footer/footer.js
@@ -6,19 +6,19 @@
                     <th>Resources</th>
                 </tr>
                 <tr class="foot">
-                    <td><a href="/hatch-www/hatch-team">Hatch Team</a></td>
-                    <td><a href="/hatch-www/guidelines">Hatch Guidelines</a></td>
-                    <td><a href="/hatch-www/about">About</a></td>
+                    <td><a href="/hatch-team">Hatch Team</a></td>
+                    <td><a href="/guidelines">Hatch Guidelines</a></td>
+                    <td><a href="/about">About</a></td>
                 </tr>
                 <tr class="foot">
-                    <td><a href="/hatch-www/contact-us">Contact Us</a></td>
+                    <td><a href="/contact-us">Contact Us</a></td>
                     <td><a href="https://discord.gg/ENTDZdaGnD">Discord Server</a></td>
-                    <td><a href="/hatch-www/privacy-policy">Privacy Policy</a></td>
+                    <td><a href="/privacy-policy">Privacy Policy</a></td>
                 </tr>
                 <tr class="foot">
                     <td><a href="https://github.com/hatchdotlol">Source Code</a></td>
                     <td><a href="https://social.hatch.lol">Social Media</a></td>
-                    <td><a href="/hatch-www/terms-of-use">Terms of Use</a></td>
+                    <td><a href="/terms-of-use">Terms of Use</a></td>
                 </tr>
             </table>
         </div>
