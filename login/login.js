51917e40f17f21efc16ff824d964db964a27c9b8
ajskateboarder <81255936+ajskateboarder@users.noreply.github.com>
2025-01-05 19:34:06 -0500

    perhaps

--- login/login.js
+++ login/login.js
@@ -1,8 +1,13 @@
-document.getElementById('submit').addEventListener('click', async () => {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+async function submit() {
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
 
-    if (username === "" || password === "") {
+    username.disabled = true;
+    password.disabled = true;
+
+    if (username.value === "" || password.value === "") {
+        username.disabled = false;
+        password.disabled = false;
         alert("Username and password is required.");
         return;
     }
@@ -13,18 +18,35 @@ document.getElementById('submit').addEventListener('click', async () => {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            username: username,
-            password: password,
+            username: username.value,
+            password: password.value,
         })
     });
 
     const json = await resp.json();
 
     if (json.message) {
+        username.disabled = false;
+        password.disabled = false;
         alert("Error: " + json.message);
         return;
     }
 
+    username.disabled = false;
+    password.disabled = false;
+
     localStorage.setItem("token", json.token);
     window.location.href = "/"
+}
+
+document.getElementById('submit').addEventListener('click', submit);
+document.getElementById('username').addEventListener('keydown', (e) => {
+    if (e.key === "Enter") {
+        submit()
+    }
+});
+document.getElementById('password').addEventListener('keydown', (e) => {
+    if (e.key === "Enter") {
+        submit()
+    }
 });
