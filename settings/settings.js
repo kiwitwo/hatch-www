af463e44388c8cb423078181049dfa83c79ac098
rayne cloudy <qwertyytheartist@gmail.com>
2025-01-11 18:57:19 -0800

    will it work

--- settings/settings.js
+++ settings/settings.js
@@ -1,7 +1,7 @@
 document.querySelector("#submit").addEventListener("click", function() {
     fetch("https://api.hatch.lol/uploads/pfp", {
         method: "POST",
-        body: document.querySelector("#avatar"),
+        body: document.querySelector("#avatar").files[0],
         headers: {
             "Token": localStorage.getItem("token")
         }
