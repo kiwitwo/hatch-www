d02b78313e244d559512733b0b6154077363b990
rayne cloudy <qwertyytheartist@gmail.com>
2025-01-11 19:23:20 -0800

    i'm committing a lot

--- settings/settings.js
+++ settings/settings.js
@@ -1,7 +1,10 @@
+const fileBlob = new Blob([document.querySelector("#avatar").files[0]], { type: 'image/png' });
+const formData = new FormData();
+
 document.querySelector("#submit").addEventListener("click", function() {
     fetch("https://api.hatch.lol/uploads/pfp", {
         method: "POST",
-        body: document.querySelector("#avatar").files[0],
+        body: formData,
         headers: {
             "Content-Type": "multipart/form-data",
             "Token": localStorage.getItem("token")
