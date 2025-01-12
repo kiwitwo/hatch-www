3152ac00bc5271b5025c686af6652f9f53a31c86
ajskateboarder <81255936+ajskateboarder@users.noreply.github.com>
2025-01-12 12:09:38 -0500

    TEST!!!

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (token) {
     }).then(res => {
         res.json().then(data => {
             document.querySelector("#welcomename").innerHTML = `Welcome back, ${data.displayName}!`;
-            document.querySelector("#welcomepfp").setAttribute("src", data.profilePicture);
+            document.querySelector("#welcomepfp").setAttribute("src", `https://api.hatch.lol${data.profilePicture}`);
             document.body.classList.remove("loading");
         })
     })
