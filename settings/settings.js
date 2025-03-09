ca5cccf0e20ca09669da698468e0261eed1f2ffd
breakfast <qwertyytheartist@gmail.com>
2025-03-09 15:37:47 -0700

    pfp

--- settings/settings.js
+++ settings/settings.js
@@ -24,12 +24,13 @@ document.querySelector("#submit").addEventListener("click", function () {
   const avatar = document.querySelector("#avatar");
   if (avatar.files[0]) {
     const formData = new FormData();
-    formData.append("file", avatar.files[0]);
+    const thumbBlob = new Blob([avatar.files[0]], { type: "image/png" });
+    formData.append("file", thumbBlob);
     fetch("https://api.hatch.lol/uploads/pfp", {
       method: "POST",
       body: formData,
       headers: {
-        "Content-Type": "multipart/form-data",
+        // "Content-Type": "multipart/form-data",
         Token: localStorage.getItem("token"),
       },
     });
