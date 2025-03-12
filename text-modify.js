b852a1b7e998d9921aaad80bacc88b93fd9593a1
breakfast <qwertyytheartist@gmail.com>
2025-03-11 18:07:26 -0700

    dash support

--- text-modify.js
+++ text-modify.js
@@ -4,7 +4,7 @@ function text_modify(text) {
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/(https?:\/\/[\S,!&lt;]+)/g, "<a href='$1'>$1</a>")
-        .replace(/@([a-z,A-Z,0-9,-,_]+)\b/g, "<a href='/user/?u=$1'>@$1</a>")
+        .replace(/@([a-z,A-Z,0-9-,_]+)\b/g, "<a href='/user/?u=$1'>@$1</a>")
         .replace(/\n/g, "<br>")
         .replace(/:glungus:/g, "<img src='/images/emojis/glungus.png' alt='glungus' class='emoji'>");
 }
