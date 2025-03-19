25af670a4e7ed64c5d217ad035fe29661da9ef01
Jabin <118082374+Jab11n@users.noreply.github.com>
2025-03-18 23:37:22 -0400

    open links in new tab

--- text-modify.js
+++ text-modify.js
@@ -3,7 +3,7 @@ function text_modify(text) {
 		.replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
-        .replace(/(https?:\/\/[\S,!&lt;]+)/g, "<a href='$1'>$1</a>")
+        .replace(/(https?:\/\/[\S,!&lt;]+)/g, "<a href='$1' target="_blank">$1</a>")
         .replace(/@([a-z,A-Z,0-9-,_]+)\b/g, "<a href='/user/?u=$1'>@$1</a>")
         .replace(/\n/g, "<br>")
         .replace(/:glungus:/g, "<img src='/images/emojis/glungus.png' alt='glungus' class='emoji'>");
