860bf6822d1866062288d144e3363a3e3d175ee3
raynecloudy <contact@raynec.dev>
2025-11-22 15:47:48 -0800

    switch /project/ and /projects/, add /p/

--- src/routes/p/[id]/+page.server.ts
+++ src/routes/p/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ params }) => {
-  redirect(308, `/project/${encodeURIComponent(params.id)}`);
+  redirect(308, `/projects/${encodeURIComponent(params.id)}`);
 };
