03e7d5af6903670c4c134ed65486a9f5d235fc87
Jab11n <jabinstech.net@gmail.com>
2026-01-10 23:14:40 -0500

    implement half a following page

---
+++ src/routes/users/[username]/following/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const { data } = $props();
+  console.log(data); //asdfghjkl
+</script>
+
+<svelte:head>
+  <title>Following - Hatch</title>
+</svelte:head>
+
+<main>hello world</main>
