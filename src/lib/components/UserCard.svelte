00cb345928c84ece0f14c3a03062902a2f3f766a
Jab11n <jabinstech.net@gmail.com>
2026-01-12 19:48:37 -0500

    User card template + followers page implementation.

---
+++ src/lib/components/UserCard.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  export let name;
+  export let displayName;
+</script>
+
+<a class="user-card" href="/users/{name}">
+  <img src="https://api.hatch.lol/users/{name}/pfp" alt="{name}'s profile picture" class="avatar" />
+  <div class="user-names">
+    <p class="user-displayname">{displayName}</p>
+    <p class="user-name">@{name}</p>
+  </div>
+</a>
+
+<style>
+  .user-card {
+    border: 1px solid var(--primary);
+    text-decoration: none !important;
+    width: 6rem;
+    height: 8rem;
+    box-sizing: border-box;
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.25rem;
+    background-color: var(--block1);
+    border-radius: 0.5rem;
+  }
+  .avatar {
+    width: 5rem;
+    height: 5rem;
+    border-radius: 0.5rem;
+  }
+  .user-names {
+    width: 100%;
+  }
+  .user-names p {
+    margin: 0;
+    font-size: 0.75rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .user-displayname {
+    color: var(--primary);
+  }
+  .user-name {
+    color: var(--bodyText);
+  }
+</style>
--- src/routes/users/[username]/followers/+page.svelte
+++ src/routes/users/[username]/followers/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import UserCard from "$lib/components/UserCard.svelte";
   const { data } = $props();
   console.log(data); //delete me once the page is made
 </script>
@@ -7,4 +8,41 @@
   <title>Followers - Hatch</title>
 </svelte:head>
 
-<main>hello world</main>
+<main>
+  <h1>{data.user.displayName}'s followers</h1>
+  <div id="fbox">
+    <div id="fbox-top">
+      <a href="/users/{data.user.name}/followers" class="fbox-link cur">Followers</a>
+      <a href="/users/{data.user.name}/following" class="fbox-link">Following</a>
+      <a href="/users/{data.user.name}/mutuals" class="fbox-link">Mutuals</a>
+    </div>
+    <hr />
+    <div id="fbox-bottom">
+      <div class="grid">
+        {#each data.followers as f}
+          <UserCard name={f.name} displayName={f.displayName} />
+        {/each}
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+  h1 {
+    font-size: 3em !important;
+  }
+  #fbox {
+    background-color: var(--block1);
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    margin-top: 1rem;
+    border-radius: 1rem;
+  }
+  .grid {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fill, 6rem);
+    justify-content: start;
+  }
+</style>
